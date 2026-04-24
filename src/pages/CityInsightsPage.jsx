9a71d849667a723a1fb79a5deb1f0a7b24a93530
Mungums <maxsboris18russ@gmail.com>
2026-04-24 15:36:16 +0400

    Add: Files for pages

---
+++ src/pages/CityInsightsPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Button from '../components/Button/Button'
+import Input from '../components/Input/Input'
+import Substrate from '../components/Substrate/Substrate'
+
+function CityInsightsPage() {
+  return (
+    <div>CityInsightsPage</div>
+  )
+}
+
+export default CityInsightsPage
