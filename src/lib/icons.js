93b757b875cdc2ad04d27166059494f3f05d7028
Mungums <maxsboris18russ@gmail.com>
2026-04-24 15:37:09 +0400

    Add: Library for icons and images

---
+++ src/lib/icons.js
@@ -0,0 +1,29 @@
+import CountIcon from '../assets/images/icons/CountIcon'
+import EmptyStarIcon from '../assets/images/icons/EmptyStarIcon'
+import FillStarIcon from '../assets/images/icons/FillStarIcon'
+import MapPointIcon from '../assets/images/icons/MapPointIcon'
+import MoonIcon from '../assets/images/icons/MoonIcon'
+import NotificationIcon from '../assets/images/icons/NotificationIcon'
+import WatchIcon from '../assets/images/icons/WatchIcon'
+import WristwatchIcon from '../assets/images/icons/WristwatchIcon'
+import BookIcon from '../assets/images/icons/BookIcon'
+import SneakersIcon from '../assets/images/icons/SneakersIcon'
+import CalendarIcon from '../assets/images/icons/CalendarIcon'
+import HistoryIcon from '../assets/images/icons/HistoryIcon'
+
+const icons = {
+    Count: CountIcon,
+    EmptyStar: EmptyStarIcon,
+    FillStar: FillStarIcon,
+    MapPoint: MapPointIcon,
+    Moon: MoonIcon,
+    Notification: NotificationIcon,
+    Watch: WatchIcon,
+    Wristwatch: WristwatchIcon,
+    Book: BookIcon,
+    Sneakers: SneakersIcon,
+    Calendar: CalendarIcon,
+    History: HistoryIcon
+}
+
+export default icons
---
+++ src/lib/images.js
@@ -0,0 +1,11 @@
+import Clouds from '../assets/images/illustrations/Clouds.png'
+import Rain from '../assets/images/illustrations/Rain.png'
+import Sun from '../assets/images/illustrations/Sun.png'
+
+const images = {
+    clouds: Clouds,
+    rain: Rain,
+    sun: Sun 
+}
+
+export default images
