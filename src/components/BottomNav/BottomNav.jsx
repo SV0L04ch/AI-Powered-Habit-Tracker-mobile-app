3890cbe59fa9f9c0d3912c95582d5c08e79b1feb
Mungums <maxsboris18russ@gmail.com>
2026-04-25 17:07:04 +0400

    Feat: base layout

--- src/components/BottomNav/BottomNav.jsx
+++ src/components/BottomNav/BottomNav.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import icons from '../../lib/icons'
+import styles from './BottomNav.module.scss'
 
-const BottomNav = () => {
+const BottomNav = ({activeTab, onTabChange}) => {
+
+  const tabs = [
+    { id: 'habits', icon: icons.Home, label: 'Главная'},
+    { id: 'insights/personal', icon: icons.Diagram, label: 'Статистика'},
+    { id: 'insights/city', icon: icons.City, label: 'Город'},
+    { id: 'profile', icon: icons.Profile, label: 'Профиль'}
+  ]
+
   return (
-    <div>BottomNav</div>
+    <nav className={styles.nav}>
+      {tabs.map((tab) => {
+        const IconComponent = tab.icon
+        const isActive = activeTab === tab.id
+
+        return (
+          <button
+            key={tab.id}
+            className={`${styles.tab} ${isActive ? styles.active : ''}`}
+            onClick={() => onTabChange(tab.id)}
+            aria-label={tab.label}
+            >
+              <IconComponent className={styles.icon}></IconComponent>
+          </button>
+        )
+      })}
+    </nav>
   )
 }
 
