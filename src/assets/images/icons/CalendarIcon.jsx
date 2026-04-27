2ffeb559992328f1f52073f92f1d52ba14091fb7
Mungums <maxsboris18russ@gmail.com>
2026-04-27 18:14:57 +0400

    Fix syntax for changing color

--- src/assets/images/icons/CalendarIcon.jsx
+++ src/assets/images/icons/CalendarIcon.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 const CalendarIcon = (props) => {
   return (
     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
-    <path d="M19 4H5C3.89543 4 3 4.89543 3 6V20C3 21.1046 3.89543 22 5 22H19C20.1046 22 21 21.1046 21 20V6C21 4.89543 20.1046 4 19 4Z" stroke="#DDEFF2" stroke-width="2"/>
-    <path d="M16 2V6" stroke="#DDEFF2" stroke-width="2" stroke-linecap="round"/>
-    <path d="M8 2V6" stroke="#DDEFF2" stroke-width="2" stroke-linecap="round"/>
-    <path d="M3 10H21" stroke="#DDEFF2" stroke-width="2"/>
+    <path d="M19 4H5C3.89543 4 3 4.89543 3 6V20C3 21.1046 3.89543 22 5 22H19C20.1046 22 21 21.1046 21 20V6C21 4.89543 20.1046 4 19 4Z" stroke="#DDEFF2" strokeWidth="2"/>
+    <path d="M16 2V6" stroke="#DDEFF2" strokeWidth="2" strokeLinecap="round"/>
+    <path d="M8 2V6" stroke="#DDEFF2" strokeWidth="2" strokeLinecap="round"/>
+    <path d="M3 10H21" stroke="#DDEFF2" strokeWidth="2"/>
     <path d="M7 15C7.55228 15 8 14.5523 8 14C8 13.4477 7.55228 13 7 13C6.44772 13 6 13.4477 6 14C6 14.5523 6.44772 15 7 15Z" fill="#DDEFF2"/>
     <path d="M12 15C12.5523 15 13 14.5523 13 14C13 13.4477 12.5523 13 12 13C11.4477 13 11 13.4477 11 14C11 14.5523 11.4477 15 12 15Z" fill="#DDEFF2"/>
     <path d="M17 15C17.5523 15 18 14.5523 18 14C18 13.4477 17.5523 13 17 13C16.4477 13 16 13.4477 16 14C16 14.5523 16.4477 15 17 15Z" fill="#DDEFF2"/>
--- src/assets/images/icons/CountIcon.jsx
+++ src/assets/images/icons/CountIcon.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const CountIcon = (props) => {
   return (
     <svg width="25" height="27" viewBox="0 0 25 27" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
-    <path d="M1.5 9.50012H22.8333M1.5 17.5001H22.8333M9.5 1.50012L6.83333 25.5001M17.5 1.50012L14.8333 25.5001" stroke="#133348" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M1.5 9.50012H22.8333M1.5 17.5001H22.8333M9.5 1.50012L6.83333 25.5001M17.5 1.50012L14.8333 25.5001" stroke="currentColor" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round"/>
     </svg>
 
   )
