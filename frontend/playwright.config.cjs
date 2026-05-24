57cd600177c795ba98524094c77ac5b0315447c3
Evgeniy Dmitriev <evgeniydmit13@gmail.com>
2026-05-24 21:33:22 +0400

    Initial playwright.config.cjs

---
+++ frontend/playwright.config.cjs
@@ -0,0 +1,49 @@
+const { defineConfig, devices } = require('@playwright/test');
+
+module.exports = defineConfig({
+  testDir: './end-to-end-tests/scenarious',
+
+  timeout: 30000,
+
+  fullyParallel: false,
+
+  forbidOnly: !!process.env.CI,
+
+  retries: process.env.CI ? 2 : 0,
+
+  workers: 1,
+
+  use: {
+    baseURL: 'http://localhost:5173',
+    headless: !!process.env.CI,
+    trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+  },
+
+  projects: [
+    {
+      name: 'chromium',
+      use: {
+        ...devices['Desktop Chrome'],
+      },
+    },
+    {
+      name: 'firefox',
+      use: {
+        ...devices['Desktop Firefox'],
+      },
+    },
+    {
+      name: 'webkit',
+      use: {
+        ...devices['Desktop Safari'],
+      },
+    },
+  ],
+
+  reporter: [
+    ['html'],
+    ['list'],
+  ]
+});
