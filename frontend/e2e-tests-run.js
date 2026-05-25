e37b674768e9118a1d074c5eb2b421cfd12b9f94
Evgeniy Dmitriev <evgeniydmit13@gmail.com>
2026-05-25 13:04:33 +0400

    feat: add smoke tests's run

--- frontend/e2e-tests-run.js
+++ frontend/e2e-tests-run.js
@@ -7,6 +7,12 @@ const testFiles = [
   'end-to-end-tests/scenarious/profile.spec.js',
 ];
 
+const smokeTestFiles = [
+  'end-to-end-tests/smoke/homepage.spec.js',
+  'end-to-end-tests/smoke/login-ui.spec.js',
+  'end-to-end-tests/smoke/responsive.spec.js'
+];
+
 let hasErrors = false;
 
 for (const file of testFiles) {
@@ -24,4 +30,23 @@ if (hasErrors) {
   process.exit(1);
 } else {
   console.log('✅ All tests passed in order!');
+}
+
+let smokeHasErrors = false;
+
+for (const file of smokeTestFiles) {
+  console.log(`\n🚀 Running ${file}...`);
+  try {
+    execSync(`npx playwright test ${file} --workers=1`, { stdio: 'inherit' });
+  } catch (error) {
+    console.error(`❌ Smoke tests failed in ${file}`);
+    smokeHasErrors = true;
+  }
+}
+
+if (hasErrors) {
+  console.error('❌ Some smoke tests failed. See logs above.');
+  process.exit(1);
+} else {
+  console.log('✅ All smoke tests passed in order!');
 }
