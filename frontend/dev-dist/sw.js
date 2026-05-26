d4b89a19b7eba2b8d414a719cd391f7f86c546d9
jakepz12 <ilarion200072@gmail.com>
2026-05-26 19:52:52 +0400

    update icons color on bottomnav

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-970124e6'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.qs93re97jj"
+    "revision": "0.ljne6fm658o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
