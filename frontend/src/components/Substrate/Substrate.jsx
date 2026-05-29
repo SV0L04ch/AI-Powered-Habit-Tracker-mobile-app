ef4185c996d7c434ebc92da9bad9dfe56f2eca0f
Mungums <maxsboris18russ@gmail.com>
2026-05-29 12:42:04 +0400

    add dynamic change classes

--- frontend/src/components/Substrate/Substrate.jsx
+++ frontend/src/components/Substrate/Substrate.jsx
@@ -3,13 +3,13 @@ import styles from './Substrate.module.scss'
 import images from '../../lib/images'
 import icons from '../../lib/icons'
 
-const Substrate = ({children, variant = "main", icon, image, alt = "Картинка:)"}) => {
-    const className = `${styles.sub} ${styles[variant] || ''}`
+const Substrate = ({children, variant = "main", icon, image, alt = "Картинка:)", className}) => {
+    const finalClassName = `${styles.sub} ${styles[variant] || ''} ${className || ''}`.trim()
     const imgSrc = images[image]
     const IconComponent = icons[icon]
 
   return (
-    <div className={className}>
+    <div className={finalClassName}>
 
       {IconComponent && <IconComponent className={styles.icon}/>}
 
